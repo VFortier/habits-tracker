0f81ec2bb866c27a633338c92b39a27ced348ad1
VFortier <vincentf26@gmail.com>
2020-10-21 22:30:06 +0100

    Minimal DB scripts, one table and one row of data

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,1 +1,13 @@
-Commit test
+DROP DATABASE IF EXISTS habits_tracker;
+
+CREATE DATABASE habits_tracker;
+
+USE habits_tracker;
+
+CREATE TABLE goal_type (
+	id           MEDIUMINT NOT NULL AUTO_INCREMENT,
+	name         VARCHAR(40),
+	description  VARCHAR(200),
+    PRIMARY KEY (id)
+);
+
---
+++ db/insert_dummy_data.sql
@@ -0,0 +1,4 @@
+USE habits_tracker;
+
+INSERT INTO goal_type (name, description)
+VALUES ("Meditation", "Mindful meditation");
