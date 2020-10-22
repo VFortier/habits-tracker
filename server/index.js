78dbf68d9bc603aaee5908fdd3d8406d6a4ae207
VFortier <vincentf26@gmail.com>
2020-10-22 23:48:03 +0100

    Basic test connection to mysql from the server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
 // Import the library
 const server = require('server');
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'habits-tracker',
+  password: 'c9025cc0fd86ff0',
+  database: 'habits_tracker',
+  insecureAuth : true
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
 // Launch the server to always answer "Hello world"
 server(ctx => 'Hello world!');
