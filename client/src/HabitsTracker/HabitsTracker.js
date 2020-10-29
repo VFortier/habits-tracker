7f8270e59df56bcf5c7d7584ed50cf124327dff3
VFortier <vincentf26@gmail.com>
2020-10-29 23:12:07 +0100

    Frontend sending a dummy request to the backend and displays the response

--- client/src/HabitsTracker/HabitsTracker.js
+++ client/src/HabitsTracker/HabitsTracker.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 
-function HabitsTracker() {
-  return (
-    <div className="App" class="bg-dark text-light">
-      ALLOOOOO
-    </div>
-  );
+class HabitsTracker extends Component {
+  state = {
+    response: "No response"
+  };
+  
+  componentDidMount() {
+    axios.get('/user/dummy@user.com').then((res) => {
+      const response = res.data.email;
+      this.setState({response});
+    });
+  }
+
+  render() {
+    return (
+      <div className="HabitsTracker">
+        <h1>Hello from the frontend!</h1>
+        <h1>{this.state.response}</h1>
+      </div>
+    );
+  }
 }
 
 export default HabitsTracker;
