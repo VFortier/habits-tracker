1bfbb650967e21985b4cf63184c9cebd3a49b96c
VFortier <vincentf26@gmail.com>
2020-11-02 15:57:09 +0000

    Have our login endpoint return a JWT token

--- server/api/controllers/user.controller.js
+++ server/api/controllers/user.controller.js
@@ -16,20 +16,19 @@ exports.signup = function(req, res) {
     return;
   }
 
-  User.findByEmail(email, (err, data) => {
-    if (data != null) {
+  User.findByEmail(email, (err, user) => {
+    if (user != null) {
       res.status(400).send({
         message: `A user with the email '${email}' already exists`
       });
     } else {
       bcrypt.hash(pwd, authConfig.SALT_ROUNDS, (err, hash) => {
-        User.add(email, hash, nickname, (err, data) => {
+        User.add(email, hash, nickname, (err, user) => {
           if (err) {
             res.status(500).send({
               message: `Error signing up in User`
             });
           } else {
-            console.log(data);
             res.send();
           }
         });
@@ -39,7 +38,7 @@ exports.signup = function(req, res) {
 };
 
 exports.login = function(req, res) {
-  User.findByEmail(req.body.email, (err, data) => {
+  User.findByEmail(req.body.email, (err, user) => {
     if (err) {
       if (err.kind === "not_found") {
         res.setHeader('WWW-Authenticate', `No existing user with email ${req.body.email}`);
@@ -50,13 +49,22 @@ exports.login = function(req, res) {
         });
       }
     } else {
-      let pwdFromDB = data.password;
+      let pwdFromDB = user.password;
 
-      bcrypt.compare(req.body.password, pwdFromDB, function(err, result) {
+      bcrypt.compare(req.body.password, pwdFromDB, function(err, doesPwdMatch) {
         // TODO - Check for err
+        console.log(authConfig);
 
-        if (result) {
-          res.send();
+        if (doesPwdMatch) {
+          var token = jwt.sign({ id: user.id }, authConfig.SECRET_KEY, {
+            expiresIn: 86400 // 24 hours
+          });
+
+          res.send({
+            email: user.email,
+            nickname: user.nickname,
+            token: token,
+          });
         } else {
           res.setHeader('WWW-Authenticate', `Wrong password for user with email ${req.body.email}`);
           res.status(401).send();
--- server/config/auth.config.js
+++ server/config/auth.config.js
@@ -1,3 +1,4 @@
 module.exports = {
 	SALT_ROUNDS: 10,
+	SECRET_KEY: "46718dca-70a7-453c-b592-92b44530777a"
 };
